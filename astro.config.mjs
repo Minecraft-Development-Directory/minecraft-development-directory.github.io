9ed8c1acbeec004068f4210548cc0cea472dd684
Louis Bailleau <louis13.bailleau@gmail.com>
2025-07-27 21:30:40 +0200

    feat: install a bunch of plugins for starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,84 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightThemeFlexoki from 'starlight-theme-flexoki'
-import starlightBlog from 'starlight-blog'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeFlexoki from "starlight-theme-flexoki";
+import starlightBlog from "starlight-blog";
+import starlightLinksValidator from "starlight-links-validator";
+import starlightImageZoom from "starlight-image-zoom";
+import starlightHeadingBadges from "starlight-heading-badges";
+import starlightSidebarTopics from "starlight-sidebar-topics";
+import starlightVideos from "starlight-videos";
+import starlightKbd from "starlight-kbd";
+import starlightScrollToTop from "starlight-scroll-to-top";
+import starlightAutoDrafts from "starlight-auto-drafts";
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://minecraft-development-directories.github.io',
-	integrations: [
-		starlight({
-			title: 'Minecraft Development Directories',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Minecraft-Development-Directory' }],
-			plugins: [starlightThemeFlexoki(), starlightBlog()],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://minecraft-development-directory.github.io",
+  integrations: [
+    starlight({
+      title: "Minecraft Development Directories",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/Minecraft-Development-Directory",
+        },
+      ],
+      plugins: [
+        starlightThemeFlexoki({ accentColor: "green" }),
+        starlightBlog(),
+        starlightLinksValidator(),
+        starlightImageZoom(),
+        starlightHeadingBadges(),
+        starlightSidebarTopics([
+          {
+            label: "Mods",
+            link: "/mods",
+            items: [
+              {
+                label: "Guides",
+                items: [
+                  // Each item here is one entry in the navigation menu.
+                  { label: "Example Guide", slug: "guides/example" },
+                ],
+              },
+              {
+                label: "Reference",
+                autogenerate: { directory: "reference" },
+              },
+            ],
+          },
+          {
+            label: "Plugins",
+            link: "/plugins",
+            items: [],
+          },
+          {
+            label: "Launcher",
+            link: "/launcher",
+            items: [],
+          },
+        ]),
+        starlightVideos(),
+        starlightKbd({
+          types: [
+            { id: "mac", label: "macOS" },
+            { id: "windows", label: "Windows", default: true },
+            { id: "linux", label: "Linux" },
+          ],
+        }),
+      ],
+      components: {
+        // Override the default `Sidebar` component with a custom one.
+        Sidebar: "./src/components/Sidebar.astro",
+      },
+    }),
+    starlightScrollToTop(),
+    starlightAutoDrafts(),
+  ],
+  vite: {
+    plugins: [Icons({ compiler: 'astro'})]
+  }
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,20 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
-import { blogSchema } from 'starlight-blog/schema';
+import { defineCollection } from "astro:content";
+import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
+import { blogSchema } from "starlight-blog/schema";
+import { videosSchema } from 'starlight-videos/schemas'
 
 export const collections = {
-	docs: defineCollection({
-		loader: docsLoader(),
-		schema: docsSchema({
-			extend: (context) => blogSchema(context)
-		})
-	}),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema(),
+  }),
+  blogs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({ extend: blogSchema }),
+  }),
+  videos: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({ extend: videosSchema }),
+  }),
 };
