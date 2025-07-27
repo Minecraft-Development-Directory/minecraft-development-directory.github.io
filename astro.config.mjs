1715fb7698deae762b9b63acc89ef1da94723114
Louis Bailleau <louis13.bailleau@gmail.com>
2025-07-27 20:16:54 +0200

    feat: configure doc theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Minecraft Development Directories',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Minecraft-Development-Directories' }],
+			plugins: [starlightThemeFlexoki()],
 			sidebar: [
 				{
 					label: 'Guides',
