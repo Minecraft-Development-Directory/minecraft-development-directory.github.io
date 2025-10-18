359fed1e75e03056d395f10079f98a5447cc6727
Louis Bailleau <louis13.bailleau@gmail.com>
2025-10-18 22:35:49 +0000

    chore: configure docus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  extends: ["docus"],
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
-})
+  modules: [
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/ui",
+    "@nuxtjs/i18n",
+  ],
+});
