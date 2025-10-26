35fcb3efa4b85f73b56cf3d68b45f18f42b87960
Louis Bailleau <louis13.bailleau@gmail.com>
2025-10-26 23:11:11 +0000

    feat: group pages by categories to only show the current group pages

--- app/composables/useNavigation.ts
+++ app/composables/useNavigation.ts
@@ -1,13 +1,78 @@
 import type { ContentNavigationItem } from "@nuxt/content";
-import { findPageBreadcrumb } from "@nuxt/content/utils";
+import { findPageBreadcrumb, findPageChildren } from "@nuxt/content/utils";
 import { mapContentNavigation } from "@nuxt/ui/utils/content";
+
+function groupChildrenByCategory(
+  items: ContentNavigationItem[],
+  slug: string
+): ContentNavigationItem[] {
+  if (!items.length) {
+    return [];
+  }
+
+  const groups: ContentNavigationItem[] = [];
+
+  const categorized: Record<string, ContentNavigationItem[]> = {};
+  const uncategorized: ContentNavigationItem[] = [];
+
+  // Remove icons while grouping
+  for (const item of items) {
+    if (item.category) {
+      categorized[item.category as string] =
+        categorized[item.category as string] || [];
+      categorized[item.category as string]?.push(item);
+    } else {
+      uncategorized.push(item);
+    }
+  }
+
+  if (uncategorized.length) {
+    const withChildren = uncategorized
+      .filter((item) => item.children?.length)
+      ?.map((item) => ({
+        ...item,
+        children: item.children?.map((child) => ({
+          ...child,
+          icon: undefined,
+        })),
+      }));
+    const withoutChildren = uncategorized.filter(
+      (item) => !item.children?.length
+    );
+
+    if (withoutChildren.length) {
+      groups.push({
+        title: "Overview",
+        path: `/guides/${slug}`,
+        children: withoutChildren?.map((item) => ({
+          ...item,
+          path: item.path,
+          icon: undefined,
+        })),
+      });
+    }
+
+    groups.push(...withChildren);
+  }
+
+  /*for (const category of categories[slug as keyof typeof categories] || []) {
+    if (categorized[category.id]?.length) {
+      groups.push({
+        title: category.title,
+        path: `/docs/${slug}`,
+        class: 'framework' in category ? [`${category.framework}-only`] : undefined,
+        children: categorized[category.id]
+      })
+    }
+  }*/
+
+  return groups;
+}
 
 function processNavigationItem(
   item: ContentNavigationItem,
   parent?: ContentNavigationItem
 ): ContentNavigationItem | ContentNavigationItem[] {
-  const localePath = useLocalePath();
-
   if (item.shadow) {
     return (
       item.children?.flatMap((child) => processNavigationItem(child, item)) ||
@@ -15,6 +80,7 @@ function processNavigationItem(
     );
   }
 
+  const localePath = useLocalePath();
   return {
     ...item,
     path: localePath(item.path),
@@ -30,12 +96,29 @@ function processNavigationItem(
 export const useNavigation = (
   navigation: Ref<ContentNavigationItem[] | undefined>
 ) => {
+  const { localePath } = useMddI18n();
+
   const rootNavigation = computed(
     () =>
       navigation.value?.[0]?.children?.map((item) =>
         processNavigationItem(item)
       ) as ContentNavigationItem[]
   );
+
+  const navigationByCategory = computed(() => {
+    const route = useRoute();
+
+    const slug = route.params.slug?.[0] as string;
+    const children = findPageChildren(
+      navigation?.value,
+      localePath(`/guides/${slug}`),
+      {
+        indexAsChild: true,
+      }
+    );
+
+    return groupChildrenByCategory(children, slug);
+  });
 
   function findBreadcrumb(path: string) {
     const breadcrumb = findPageBreadcrumb(navigation?.value, path, {
@@ -47,6 +130,7 @@ export const useNavigation = (
 
   return {
     rootNavigation,
+    navigationByCategory,
     findBreadcrumb,
   };
 };
