ea812b3e2c6592ff41ee95c97bd918b8d72baced
Louis Bailleau <louis13.bailleau@gmail.com>
2025-10-26 10:29:25 +0000

    chore: add a TODO message to `guide/[...slug].vue`

--- app/pages/[[lang]]/guides/[...slug].vue
+++ app/pages/[[lang]]/guides/[...slug].vue
@@ -1,13 +1,7 @@
 <template>
-  <div>
-    
-  </div>
+  <div>TODO</div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
